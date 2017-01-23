1a147ca9b496509ac7934eee5aa61a7196afa8f1
dhill92 <dhill92@gmail.com>
2017-01-22 19:15:30 -0500

    initial-commit

---
+++ code.rb
@@ -0,0 +1,11 @@
+#adults:12.80
+#children:4.00
+
+adults = 12.8
+children = 4
+adult_number = 4
+children_number = 2
+
+total = adults * adult_number + children * children_number
+
+puts "the total cost will be $#{total}."
